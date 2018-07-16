efae375ab271149037cc4f7888f470d22fd39fb5
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-15 17:37:31 -0700

    Some of the further study

--- melons.py
+++ melons.py
@@ -1,8 +1,12 @@
 """Classes for melon orders."""
 
+from random import randint
+from datetime import datetime
+import time
+
 class AbstractMelonOrder():
 
-    def __init__(self, species, qty,country_code=None, is_christmas= False):
+    def __init__(self, species, qty,country_code = None, is_christmas = False):
         """Initialize melon order attributes."""
 
         self.species = species
@@ -10,24 +14,42 @@ class AbstractMelonOrder():
         self.shipped = False
         if country_code:
             self.country_code = country_code
-        if is_christmas:
-            self.is_christmas = is_christmas
+        self.is_christmas = is_christmas
 
 
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        #base_price = 5
+        total = (1 + self.tax) * self.qty * self.get_base_price()
 
         if self.is_christmas == True:
             total = (total*1.5)
+        
 
         if self.order_type == 'international' and self.qty < 10:
             total += 3
 
         return total
 
+
+    def get_base_price(self):
+        base_price = randint(5,10) 
+
+        current_stamp = datetime.now().timestamp()
+        readable_cur_stamp = time.ctime(current_stamp)
+
+        time_lst = readable_cur_stamp.split(' ')
+        hour_lst = time_lst[3].split(':')
+
+        if time_lst[0] == "Sat" or time_lst[0] == "Sun":
+            return base_price
+        elif int(hour_lst[0]) >= 8 and int(hour_lst[0]) < 11:
+            if int(hour_lst[1]) >= '00' and int(hour_lst[1]) <= 59:
+                base_price +=4
+            return base_price
+        else:
+            return base_price
 
 
 
